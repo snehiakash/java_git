dcdd147fabae5d6acad82a8405b8d1ea1016d42f
snehiakash <91912919+snehiakash@users.noreply.github.com>
2024-08-12 01:27:38 -0700

    Delete java_git directory

--- java_git/Countwords.java
+++
@@ -1,7 +0,0 @@
-class Countwords{
-public static void main(String [] args){
-String str="Iam Learning Java";
-long count =str.split("\\s+").length;
-System.out.println(count);
-}
-} 
--- java_git/PrintAlternativeNumber.java
+++
@@ -1,8 +0,0 @@
-class PrintAlternativeNumber{
-public static void main(String [] args){
-
-int [] array={1,2,3,4,5,6};
-for(int i=0;i<array.length;i=i+2){
-System.out.println(array[i]);}
-}
-}
